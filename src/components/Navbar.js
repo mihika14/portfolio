74d44dcf36faed4af5d31fa46e680b1ea3442358
mihika14 <mihikasaxena13@gmail.com>
2023-02-06 00:28:50 +0530

    changes2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,9 @@
 import React from "react";
-import { Menu } from 'semantic-ui-react';
-import { Link } from "react-router-dom";
-
-export default class Navbar extends React.Component {
-   state = {}
- 
-   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
- 
-   render() {
-     const { activeItem } = this.state
- 
-     return (
-       <Menu>
-        
-         <Menu.Item
-           as={ Link } to='skills'
-           name='skills'
-           active={activeItem === 'skills'}
-           onClick={this.handleItemClick}
-         >     
-         </Menu.Item>
- 
-         <Menu.Item
-           as={ Link } to='about'
-           name='about'
-           active={activeItem === 'about'}
-           onClick={this.handleItemClick}
-         > About
-         </Menu.Item>
- 
-         <Menu.Item
-           as={ Link } to='education'
-           name='Education'
-           active={activeItem === 'Education'}
-           onClick={this.handleItemClick}
-         >
-          Education
-         </Menu.Item>
-       </Menu>
-     )
-   }
- }
+export default function Navbar () {
+  return(
+      <div class = "ui three item menu" >
+      <a class="active item">Editorials</a>
+     <a class="item">Reviews</a>
+     <a class="item">Upcoming Events</a>
+    </div>
+)}
