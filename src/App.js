da921b3aaae7235e8beb9c265c447ce567d1cfb6
mihika14 <mihikasaxena13@gmail.com>
2023-02-22 23:00:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomePage from "./components/HomePage/Homepage";
-
-
+import "./App.css";
 
 export default function App() {
-return(
-   <div ><HomePage /></div>
 
-)
-
-
-}
+      const [isLoading, setIsLoading] = useState(true);
+    
+      useEffect(() => {
+        // Simulate a loading delay for 2 seconds
+        setTimeout(() => setIsLoading(false), 2000);
+      }, []);
+    
+      return (
+        <>
+          {isLoading ? (
+            <div className="loader">
+              <div className="waves"></div>
+            </div>
+          ) : (
+            <div>
+              <HomePage />
+            </div>
+          )}
+        </>
+      );
+    }
+   
+ 
