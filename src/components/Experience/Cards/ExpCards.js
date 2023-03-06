9c579e0ab951508ab9b47875eb9e1bfbb65a33b0
mihika14 <mihikasaxena13@gmail.com>
2023-03-07 01:05:41 +0530

    hehejsf

--- src/components/Experience/Cards/ExpCards.js
+++ src/components/Experience/Cards/ExpCards.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import "./ExpCard.css";
+import simmi from "./assests/simmi.png";
+import vidyut from "./assests/vidyut.png";
+import react from "./assests/react.png";
+import mongo from "./assests/mongo.png";
+import python from "./assests/pytjom.png";
 
 const ExpCard = () => {
   const details = [
     {
+      image:simmi,
       title: "REACT DEVELOPER",
       organization: "Simmi Foundation",
       date:"(Sept 2022-Dec 2022)",
@@ -12,76 +18,50 @@ const ExpCard = () => {
       
     },
     {
+      image:vidyut,
       title: "CORE COUNCIL",
       organization: "Vidyut",
       body: "As a core committee member of the Vidyut-Electronics Society at SRCASW, I leveraged my skills in event management,teamwork,and leadership to successfully organize and execute multiple events.Through effective coordination, communication,I ensured the smooth functioning and played a vital role in their overall success.",
       link: "https://www.instagram.com/vidyut.srcasw/?hl=en"
     },
     {
+      image:react,
       title: "MODERN REACT WITH REDUX",
       organization: "Udemy",
       body: "This course provided me with a solid understanding of how to create complex and dynamic user interfaces using React.The course also covered state management in depth, which is crucial for building complex React applications. And how to use more advanced state management libraries such as Redux to handle complex state.",
     },
     {
+      image:mongo,
       title: "AUTHENICATION SERVER",
       organization: "Devtown",
       body: "In a seven-day workshop, I was able to develop a secure and reliable authentication server using MongoDB and Node.js . During this project, I learned how to integrate MongoDB, with Node.js. This project provided me with hands-on experience in building a secure and scalable authentication system using MongoDB and Node.js.",
       link:"https://drive.google.com/file/d/1P_XCKgmGgM0EDWYNPfWHhAQ10LLoKkTY/view?usp=sharing"
     },
     {
+      image:python,
       title:"ADVANCE PYTHON",
       organization:"CETPA",
       body:"During this workshop, I learned about the basics of Python programming, including classes, functions, and the popular data analysis library pandas. I also had the opportunity to build projects using Python's GUI library Tkinter, and also did a project i.e Zomato Food Analysis using libraries pandas and Numpy.",
       link:"https://drive.google.com/file/d/1GyhEvz8CjE4hWXr58kfetYCIwk9j4a_R/view?usp=sharing"
     }
   ]
-
-  const Card = ({ title, organization, date ,body, link }) => {
-    const handleClick = () => {
-      window.open(link, '_blank');
-    }
-
     return (
-      <div className="card">
-        <div className='card2'>
-          <h2 className='cardTitle'>{title}</h2>
-          <h3 className='organization'>{organization}</h3>
-          <h3 className="date">{date}</h3>
-          <p className='cardBody'>{body}</p>
-          <button className='box' onClick={handleClick}>
-            <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="37" cy="37" r="35.5" stroke="black" strokeWidth="3"></circle>
-              <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
-            </svg></button>
+      <div className='expcards'>
+      {details.map((detail, index) => (
+        <div className="expcard" key={index}>
+          <img className="expimage" src ={detail.image}></img>
+          <div className="expcontent">
+            <h1 className="exptitle">{detail.title}</h1>
+            <h2 className="exporganization">{detail.organization}</h2>
+            <p className="expdesc">{detail.body}</p>
+            <a className="expaction" href={detail.link}>
+              <span classname="arrow" aria-hidden="true">→</span>
+            </a>
+          </div>
         </div>
-      </div>
-    );
-  };
-
-
-  const CardList = ({ details }) => {
-    return (
-      <div className="cardList">
-        {details.map((detail, index) => (
-          <Card
-            key={index}
-            title={detail.title}
-            organization={detail.organization}
-            date={detail.date}
-            body={detail.body}
-            link={detail.link}
-          />
-        ))}
-      </div>
-    );
-  };
-
-
-  return (
-    <div>
-      <CardList details={details} />
+      ))}
     </div>
+    );
+  };
 
-  )
-}
 export default ExpCard;
